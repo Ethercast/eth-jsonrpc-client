369826d0eea8b59d6a97f3594cd97992a225bb53
moody <moody.salem@gmail.com>
2018-09-13 00:21:59 -0400

    specific modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-export * from './lib/eth-client';
-export * from './lib/eth-https-client';
-export * from './lib/eth-ws-client';
-export * from './lib/validated-eth-client';
-export * from './lib/get-client';
+import EthClient, { BlockParameter, LogFilter, Method, NumberLike, SendTransactionParameters } from './lib/eth-client';
+import EthHTTPSClient from './lib/eth-https-client';
+import EthWSClient from './lib/eth-ws-client';
+import getClient from './lib/get-client';
+import ValidatedEthClient from './lib/validated-eth-client';
+
+export {
+  EthClient,
+  EthHTTPSClient,
+  EthWSClient,
+  ValidatedEthClient,
+  getClient,
+  SendTransactionParameters, LogFilter, NumberLike, BlockParameter, Method
+};
